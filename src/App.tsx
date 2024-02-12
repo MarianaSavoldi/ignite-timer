3b0beb88d26c12c896a0e6e7322229e4faeb9cee
MarianaSavoldi <marianasavodi123@gmail.com>
2024-02-12 00:30:58 -0300

    lint: configure and apply eslint rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { ThemeProvider } from 'styled-components';
-import { defaultTheme } from './styles/themes/default';
-import { GlobalStyle } from './styles/global';
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
-    <ThemeProvider theme={ defaultTheme }>
+    <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
       <h1>Hello World</h1>
     </ThemeProvider>
-  );
+  )
 }
