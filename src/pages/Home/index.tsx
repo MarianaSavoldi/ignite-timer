218cd607cc8215ff41b704c9d9f18b3260ec0c1f
MarianaSavoldi <marianasavodi123@gmail.com>
2024-02-17 02:31:09 -0300

    feat: implement zod validation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 
 import {
   CountdownContainer,
@@ -11,13 +13,29 @@ import {
   TaskInput,
 } from './styles'
 
+const newCycleValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser de, no mínimo, 5 minutos.')
+    .max(60, 'O ciclo precisa ser de, no máximo, até 60 minutos.'),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleValidationSchema>
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm()
+  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
 
   const task = watch('task')
   const isSubmitDisabled = !task
 
-  const handleCreateNewCycle = (data: any) => {
+  const handleCreateNewCycle = (data: NewCycleFormData) => {
     console.log(data)
   }
 
